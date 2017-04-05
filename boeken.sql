3994552ce3ae5f7bb389d10512b1181ea8d08b2e
st201457 <201457@edu.rocfriesepoort.nl>
2017-04-05 13:47:06 +0200

    Add files via upload

---
+++ boeken.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 05 apr 2017 om 13:47
+-- Serverversie: 10.1.21-MariaDB
+-- PHP-versie: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `boekenlijst`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `boeken`
+--
+
+CREATE TABLE `boeken` (
+  `id` int(11) NOT NULL,
+  `boek` varchar(255) NOT NULL,
+  `uitgever` varchar(255) NOT NULL,
+  `isbn` bigint(33) NOT NULL,
+  `jaaruitgave` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `prijs` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `boeken`
+--
+
+INSERT INTO `boeken` (`id`, `boek`, `uitgever`, `isbn`, `jaaruitgave`, `status`, `prijs`) VALUES
+(1, 'Binas / 6e havo/vwo / deel informatieboek', 'Noordhoff Uitgevers B.V.', 9789001817497, 2013, 'nieuw', '555.95'),
+(2, 'Basisboek wiskunde', 'Stuttgart Bosch', 9789043016735, 2009, 'zo goed als nieuw', '601.99'),
+(3, 'Basisvaardigheden wiskunde', 'Noordhoff Uitgevers B.V.', 9789001834159, 2014, 'gebruikt', '100.00');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `boeken`
+--
+ALTER TABLE `boeken`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `boeken`
+--
+ALTER TABLE `boeken`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
