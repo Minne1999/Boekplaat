1ecdc68fb86eeeba6e41ccbeb50b0f88672d2e45
wietse1999 <200773@edu.rocfriesepoort.nl>
2017-04-11 15:03:23 +0200

    Update slider.js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -13,6 +13,8 @@ function showDivs(n) {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
     }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
     setTimeout(slideIndex, 2500);
 }
